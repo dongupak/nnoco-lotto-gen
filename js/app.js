354cf93630af647341c55a249d7dbcfad7f74fac
DongGyu Park <dongupak@gmail.com>
2020-01-02 18:57:35 -0800

    수정... 어쩌구2

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ function generate() {
 }
 
 /* check a duplicated number in array */
+//== check a duplicated number in array ==//
 function contains(numbers, number) {
   for(var i=0; i<numbers.length; i++) {
     if(numbers[i] == number) return true;
